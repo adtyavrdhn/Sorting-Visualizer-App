09247d8099015c20ce074110cb0c9e954844612e
Aditya Vardhan <adtyavrdhn@gmail.com>
2022-09-30 19:19:12 +0530

    fixed inline function error in tab screens

--- SortingVisualizer/src/App.tsx
+++ SortingVisualizer/src/App.tsx
@@ -3,38 +3,26 @@ import {NavigationContainer} from '@react-navigation/native';
 import {TailwindProvider} from 'tailwind-rn';
 import utilities from '../tailwind.json';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-// import Header from './components/Header';
-// import Towers from './components/Towers';
-// import {SafeAreaView, View, Text} from 'react-native';
 import SortScreen from './components/SortScreen';
 
 const App = () => {
   const Tab = createBottomTabNavigator();
 
+  const Bsortscreen = () => <SortScreen algo="Bubble Sort" />;
+  const Msortscreen = () => <SortScreen algo="Merge Sort" />;
+  const Qsortscreen = () => <SortScreen algo="Quick Sort" />;
+  const Isortscreen = () => <SortScreen algo="Insertion Sort" />;
+  const Ssortscreen = () => <SortScreen algo="Selection Sort" />;
+
   return (
     <TailwindProvider utilities={utilities}>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen
-            name="Bubble"
-            component={() => <SortScreen algo="Bubble Sort" />}
-          />
-          <Tab.Screen
-            name="Selection"
-            component={() => <SortScreen algo="Selection Sort" />}
-          />
-          <Tab.Screen
-            name="Merge"
-            component={() => <SortScreen algo="Merge Sort" />}
-          />
-          <Tab.Screen
-            name="Quick"
-            component={() => <SortScreen algo="Quick Sort" />}
-          />
-          <Tab.Screen
-            name="Insertion"
-            component={() => <SortScreen algo="Insertion Sort" />}
-          />
+          <Tab.Screen name="Bubble" component={Bsortscreen} />
+          <Tab.Screen name="Selection" component={Ssortscreen} />
+          <Tab.Screen name="Merge" component={Msortscreen} />
+          <Tab.Screen name="Quick" component={Qsortscreen} />
+          <Tab.Screen name="Insertion" component={Isortscreen} />
         </Tab.Navigator>
       </NavigationContainer>
     </TailwindProvider>
