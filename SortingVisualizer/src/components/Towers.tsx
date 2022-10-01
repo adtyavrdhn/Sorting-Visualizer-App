bfe06cd3e875589b1f0f43e64728c4139e324ce7
Aditya Vardhan <adtyavrdhn@gmail.com>
2022-10-01 18:13:44 +0530

    dependency update
    removed gradient dependancy

--- SortingVisualizer/src/components/Towers.tsx
+++ SortingVisualizer/src/components/Towers.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {View} from 'react-native';
 import {useTailwind} from 'tailwind-rn';
-import LinearGradient from 'react-native-linear-gradient';
 
 function Towers(props: any) {
   const tailwind = useTailwind();
