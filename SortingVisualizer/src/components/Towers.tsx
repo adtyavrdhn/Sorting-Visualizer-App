3cffbef883af10ea4ebbd53e36fbc664f0f9d403
Aditya Vardhan <adtyavrdhn@gmail.com>
2022-10-01 13:59:46 +0530

    Update Towers.tsx

--- SortingVisualizer/src/components/Towers.tsx
+++ SortingVisualizer/src/components/Towers.tsx
@@ -15,12 +15,9 @@ function Towers(props: any) {
   }
   return (
     <View style={tailwind('flex flex-col')}>
-      <View
-        style={{
-          ...tailwind('flex flex-row justify-center'),
-        }}>
+      <View style={tailwind('flex flex-row justify-center')}>
         {props.arr.map((n: number, index: number) => (
-          <View key={index} style={{...towerStyles(n)}} />
+          <View key={index} style={towerStyles(n)} />
         ))}
       </View>
     </View>
